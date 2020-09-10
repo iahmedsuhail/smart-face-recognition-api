e4cb64774b9b6a88a9e4fc2845a0047971d0a229
iahmedsuhail@gmail.com <iahmedsuhail@gmail.com>
2020-09-10 13:07:42 +1200

    Add cors for requests from client

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt-nodejs");
+var cors = require("cors");
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const database = {
   users: [
@@ -19,7 +21,8 @@ const database = {
 };
 
 app.get("/", (req, res) => {
-  res.send("Server is working");
+  console.log("App is tryna connect");
+  res.send(database.users);
 });
 
 app.post("/signin", (req, res) => {
