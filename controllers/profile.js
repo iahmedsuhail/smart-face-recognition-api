5bb3e0155a924c1e72a273da3fc1f252e2561168
iahmedsuhail@gmail.com <iahmedsuhail@gmail.com>
2020-09-11 19:38:11 +1200

    Move route handlers to controllers directory
    Better separation of concerns using dependency injection

---
+++ controllers/profile.js
@@ -0,0 +1,21 @@
+const handleProfile = (req, res, db) => {
+  const { id } = req.params;
+
+  db.select("*")
+    .from("users")
+    .where({
+      id: id,
+    })
+    .then((user) => {
+      if (user.length) {
+        res.json(user[0]);
+      } else {
+        res.status("400").json("Something went wrong");
+      }
+    })
+    .catch((err) => res.status("400").json("Something went wrong"));
+};
+
+module.exports = {
+  handleProfile: handleProfile,
+};
--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt-nodejs");
 var cors = require("cors");
+const register = require("./controllers/register.js");
+const signin = require("./controllers/signin.js");
+const profile = require("./controllers/profile.js");
+const image = require("./controllers/image.js");
 
 const app = express();
 app.use(bodyParser.json());
@@ -27,91 +31,23 @@ db.select("*")
 
 app.get("/", (req, res) => {
   console.log("Tryna connect to home route");
-  res.send(database.users);
+  // res.send(database.users);
 });
 
 app.post("/signin", (req, res) => {
-  console.log("tryna hit signin route");
-  db.select("email", "hash")
-    .from("login")
-    .where("email", "=", req.body.email)
-    .then((data) => {
-      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-      console.log("isValid from signin is", isValid);
-      if (isValid) {
-        db.select("*")
-          .from("users")
-          .where("email", "=", req.body.email)
-          .then((user) => {
-            res.json(user[0]);
-          })
-          .catch((err) => res.status("400").json("Unable to get user"));
-      } else {
-        res.status(400).json("Wrong credentials");
-      }
-    })
-    .catch((err) => res.status("400").json("Something went wrong"));
+  signin.handleSignin(req, res, db, bcrypt);
 });
 
 app.post("/register", (req, res) => {
-  console.log("tryna hit register route");
-  const { email, name, password } = req.body;
-
-  const hash = bcrypt.hashSync(password);
-
-  db.transaction((trx) => {
-    trx
-      .insert({
-        hash: hash,
-        email: email,
-      })
-      .into("login")
-      .returning("email")
-      .then((loginEmail) => {
-        return trx("users")
-          .returning("*")
-          .insert({
-            name: name,
-            email: loginEmail[0],
-            entries: 0,
-            joined: new Date(),
-          })
-          .then((user) => {
-            res.json(user[0]);
-          });
-      })
-      .then(trx.commit)
-      .catch(trx.rollback);
-  }).catch((err) => res.status(400).json("Unable to register"));
+  register.handleRegister(req, res, db, bcrypt);
 });
 
 app.get("/profile/:id", (req, res) => {
-  const { id } = req.params;
-
-  db.select("*")
-    .from("users")
-    .where({
-      id: id,
-    })
-    .then((user) => {
-      if (user.length) {
-        res.json(user[0]);
-      } else {
-        res.status("400").json("Something went wrong");
-      }
-    })
-    .catch((err) => res.status("400").json("Something went wrong"));
+  profile.handleProfile(req, res, db);
 });
 
 app.put("/image", (req, res) => {
-  const { id } = req.body;
-
-  db("users")
-    .where("id", "=", id)
-    .increment("entries", 1)
-    .returning("entries")
-    .then((entries) => res.json(entries[0]))
-    .catch((err) => res.status("400").json("Unable to get entries"));
+  image.handleImage(req, res, db);
 });
 
 app.listen(3000, () => {
