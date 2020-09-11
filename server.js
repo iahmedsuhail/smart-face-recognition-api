d0f76eb23933b7f0938ffb4921b682955c8483be
iahmedsuhail@gmail.com <iahmedsuhail@gmail.com>
2020-09-11 16:53:26 +1200

    Update register route to hash password in a SQL transaction

--- server.js
+++ server.js
@@ -57,18 +57,32 @@ app.post("/register", (req, res) => {
   console.log("tryna hit register route");
   const { email, name, password } = req.body;
 
-  db("users")
-    .returning("*")
-    .insert({
-      name: name,
-      email: email,
-      entries: 0,
-      joined: new Date(),
-    })
-    .then((user) => {
-      res.json(user[0]);
-    })
-    .catch((err) => res.status(400).json("Unable to register"));
+  const hash = bcrypt.hashSync(password);
+
+  db.transaction((trx) => {
+    trx
+      .insert({
+        hash: hash,
+        email: email,
+      })
+      .into("login")
+      .returning("email")
+      .then((loginEmail) => {
+        return trx("users")
+          .returning("*")
+          .insert({
+            name: name,
+            email: loginEmail[0],
+            entries: 0,
+            joined: new Date(),
+          })
+          .then((user) => {
+            res.json(user[0]);
+          });
+      })
+      .then(trx.commit)
+      .catch(trx.rollback);
+  }).catch((err) => res.status(400).json("Unable to register"));
 });
 
 app.get("/profile/:id", (req, res) => {
