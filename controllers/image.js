176e223b8564cc4dab7e53e6f1afed4200e6e406
iahmedsuhail@gmail.com <iahmedsuhail@gmail.com>
2020-09-11 20:06:19 +1200

    Move Api call to backend

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,18 @@
+const Clarifai = require("clarifai");
+
+const app = new Clarifai.App({
+  apiKey: "20fd979dbbae4bdf9e75eafd84161330",
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => res.status(400).json("Unable to work with the API"));
+};
+
 const handleImage = (req, res, db) => {
   const { id } = req.body;
 
@@ -11,4 +26,5 @@ const handleImage = (req, res, db) => {
 
 module.exports = {
   handleImage: handleImage,
+  handleApiCall: handleApiCall,
 };
