caff7a2f474251e9d8fef68c014712a9e11c6b3f
iahmedsuhail@gmail.com <iahmedsuhail@gmail.com>
2020-09-11 17:04:26 +1200

    Update signin route

--- server.js
+++ server.js
@@ -45,12 +45,25 @@ app.get("/", (req, res) => {
 
 app.post("/signin", (req, res) => {
   console.log("tryna hit signin route");
-  if (
-    req.body.email === database.users[0].email &&
-    req.body.password === database.users[0].password
-  ) {
-    res.json(database.users[0]);
-  } else res.json("fail");
+  db.select("email", "hash")
+    .from("login")
+    .where("email", "=", req.body.email)
+    .then((data) => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      console.log("isValid from signin is", isValid);
+      if (isValid) {
+        db.select("*")
+          .from("users")
+          .where("email", "=", req.body.email)
+          .then((user) => {
+            res.json(user[0]);
+          })
+          .catch((err) => res.status("400").json("Unable to get user"));
+      } else {
+        res.status(400).json("Wrong credentials");
+      }
+    })
+    .catch((err) => res.status("400").json("Something went wrong"));
 });
 
 app.post("/register", (req, res) => {
